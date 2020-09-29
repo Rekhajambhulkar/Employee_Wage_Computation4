247e5439e22b83a627fd5c0c0802da6406d0fc01
Rekhajambhulkar <jambhulkarrekha@gmail.com>
2020-09-29 19:27:01 +0530

    Check Employee Attendance

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -1,8 +1,26 @@
 public class EmployeeWage 
 {
+
+//CONSTANTS
+public static final int Is_Employee_Present=1;
+
 public static void main(String[] args)
 {
 //Display the Welcome Message
 System.out.println("Welcome to the EmployeeWage Program");
+
+//generate 0-2
+int employeeCheck=(int) Math.floor(Math.random()*10)%3;
+
+if( Is_Employee_Present == employeeCheck)
+{ 
+	System.out.println("Employee is present");
+}
+else
+{ 
+	System.out.println("Employee is Absent");
+
+}
+
 }
 }
